ef6489faf2b85c21f455494fb5297e489ae66af5
vaishnavi paliya <paliyavaishnavi11@gmail.com>
2021-04-21 17:26:51 +0530

    added event listeners

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("this file is working")
+var btnConvert= document.querySelector("#btn-convert")
+var txtInput = document.querySelector("#txt-input")
+
+console.log(txtInput)
+
+function clickHandler(){
+    console.log("button got clicked!!")
+    console.log("input",txtInput.value)
+}
+
+btnConvert.addEventListener("click", clickHandler)
